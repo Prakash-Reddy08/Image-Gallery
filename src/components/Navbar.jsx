50b7ae53567f49c9f8d0ae7f6f091624026d9970
Prakash-Reddy08 <mjprakashreddy1@gmail.com>
2022-05-11 19:39:39 +0530

    create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,34 @@
-import { AppBar, styled, Toolbar, Typography } from "@mui/material"
+import { AppBar, Autocomplete, TextField, Toolbar, Typography } from "@mui/material"
+import styled from '@emotion/styled'
+const dummyData = ["HTML", "CSS", "javaScript", "TypeScript"];
 
 const Navbar = () => {
     return (
-        <AppBar position="static" sx={{ backgroundColor: "#FF44FF", boxShadow: "none", height: "97px", justifyContent: "center" }}>
-            <StyledToolbar>
-                <Typography variant="h4" sx={{ fontFamily: 'Pattaya', color: "#333333" }}>
-                    Image Galary
-                </Typography>
-            </StyledToolbar>
-        </AppBar >
+        <Wrapper>
+            <AppBar position="sticky" sx={{ backgroundColor: "#FFFFFF", boxShadow: "none", height: "97px", justifyContent: "center" }}>
+                <StyledToolbar>
+                    <div className="left">
+                        <Typography variant="h4" sx={{ fontFamily: 'Pattaya', color: "#333333" }}>
+                            Image Galary
+                        </Typography>
+                    </div>
+                    <StyledMid>
+                        <Autocomplete freeSolo sx={{ width: "419px" }} options={dummyData} renderInput={(params) => <TextField {...params} label="Search Images here" />} />
+                        <StyledUL>
+                            <li>Explore</li>
+                            <li>Collection</li>
+                            <li>Community</li>
+                        </StyledUL>
+                    </StyledMid>
+                    <div className="right">
+                        <StyledMode>
+                            Dark Mode
+                        </StyledMode>
+                    </div>
+                </StyledToolbar>
+            </AppBar >
+        </Wrapper>
+
     )
 }
 
@@ -18,5 +38,25 @@ const StyledToolbar = styled(Toolbar)({
     justifyContent: "space-between",
     margin: "0 150px 0 150px"
 })
+
+const StyledMid = styled("div")({
+    display: "flex",
+})
+const StyledUL = styled("ul")({
+    color: "#333333",
+    listStyleType: "none",
+    display: "flex",
+    justifyContent: "space-between",
+    width: "400px",
+    fontWeight: "700",
+})
+const StyledMode = styled("div")({
+    color: "#333333",
+    fontWeight: "700",
+})
+const Wrapper = styled.div`
+
+`
+
 
 export default Navbar
